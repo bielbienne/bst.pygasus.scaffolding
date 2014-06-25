688c9177f2793bc2b0978ef29b7e70c3bd909fd7
riols <riols@9e2e370e-7998-ad43-8ce4-5fe541838c8f>
2014-06-25 16:01:37 +0000

    add base class

--- src/bb/extjs/scaffolding/fields/model.py
+++ src/bb/extjs/scaffolding/fields/model.py
@@ -5,28 +5,33 @@ from bb.extjs.scaffolding.fields import BuilderBase
 from bb.extjs.scaffolding.interfaces import IScaffoldingRecipeModel
 
 
+class ModelBuilderBase(BuilderBase):
+    ext.baseclass()
+    
+    def base(self, overrides):
+        b = dict(name = self.field.getName(),
+                 useNull = not self.field.required)
+        b.update(overrides)
+        return b
 
-class StringField(BuilderBase):
+
+class StringField(ModelBuilderBase):
     ext.adapts(IScaffoldingRecipeModel, schema.interfaces.IField)
     def __call__(self):
-        return dict(name=self.field.getName(),
-                    type='string')
+        return self.base(dict(type='string'))
 
 
-class DateField(BuilderBase):
+class DateField(ModelBuilderBase):
     ext.adapts(IScaffoldingRecipeModel, schema.interfaces.IDate)
     def __call__(self):
-        return dict(name=self.field.getName(),
-                    type='date',
-                    dateFormat='Y-m-d H:i:s.u')
+        return self.base(dict(type='date',
+                              dateFormat='Y-m-d H:i:s.u'))
 
 
-class IntField(BuilderBase):
+class IntField(ModelBuilderBase):
     ext.adapts(IScaffoldingRecipeModel, schema.interfaces.IInt)
     def __call__(self):
-        return dict(name=self.field.getName(),
-                    type='int',
-                    useNull=True)
+        return self.base(dict(type='int'))
 
 class IdField(IntField):
     ext.adapts(IScaffoldingRecipeModel, schema.interfaces.IId)
